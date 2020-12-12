b4c8aefdbcad3def8380a058be1693ca488b21ab
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-12-12 15:43:30 +0200

    BlogController needs a PostRepository property

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,11 +1,13 @@
 package com.pluralsight.blog;
 
+import com.pluralsight.blog.data.PostRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BlogController {
+    private PostRepository postRepository;
 
     @RequestMapping("/")
     public String listPosts(ModelMap modelMap) {

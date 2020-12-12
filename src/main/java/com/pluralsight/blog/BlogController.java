7a7556266907107f3d495b3bbba038a3dac5513a
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-12-12 19:51:49 +0200

    Pass the Post to the template

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -27,6 +27,7 @@ public class BlogController {
     @RequestMapping("/post/{id}")
     public String postDetails(@PathVariable Long id, ModelMap modelMap) {
         Post post = postRepository.findById(id);
+        modelMap.put("post", post);
         return "post-details";
     }
 }

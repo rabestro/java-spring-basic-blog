15841a09b80cdf4a0bb584869fb613066feb22c5
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-12-12 15:22:08 +0200

    Adding @RequestMapping Annotation

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,9 +1,12 @@
 package com.pluralsight.blog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BlogController {
+
+    @RequestMapping("/")
     public String listPosts() {
         return "home";
     }
